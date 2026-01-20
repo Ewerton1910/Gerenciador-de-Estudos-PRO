10ef923a413a0cc9b8eec79047f5065137a3e9fe
Ewerton1910 <80080887+Ewerton1910@users.noreply.github.com>
2026-01-20 05:33:20 -0300

    Update _middleware.js

--- _middleware.js
+++ _middleware.js
@@ -1,30 +1,29 @@
 import { next } from '@vercel/edge';
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  // Isso garante que o middleware rode em todas as páginas
+  matcher: '/:path*',
 };
 
 export default function middleware(req) {
   const authHeader = req.headers.get('authorization');
 
   if (authHeader) {
-    // Decodifica o Usuário e Senha enviados pelo navegador
     const authValue = authHeader.split(' ')[1];
+    // Decodifica o login (admin:suasenha)
     const [user, pwd] = atob(authValue).split(':');
 
-    const usuarioCorreto = 'admin'; // Você pode mudar o nome de usuário aqui
-    const senhaCorreta = process.env.SITE_PASSWORD || '1234';
-
-    if (user === usuarioCorreto && pwd === senhaCorreta) {
+    // Compara com a variável que você salvou na Vercel
+    if (user === 'admin' && pwd === process.env.SITE_PASSWORD) {
       return next();
     }
   }
 
-  // Se não estiver logado, envia o comando para o navegador abrir o pop-up
   return new Response('Acesso Restrito', {
     status: 401,
     headers: {
       'WWW-Authenticate': 'Basic realm="Acesso Protegido"',
+      'Content-Type': 'text/html; charset=utf-8'
     },
   });
 }
